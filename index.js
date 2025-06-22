08be708f09668d960fc1d1c083a22d1ed1fa7623
Sandalizzz <sandaliranahewa@gmail.com>
2025-06-23 01:49:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let name = "sandaliz"
 
 let favoriteActivity = "watching  movies and tv series"
 
-let favoritePlace = "anywhere with my mom or best friends"
+let favoritePlace = "anywhere with my favourite people"
 
 // The higher temperature, the more random & experimental output(from 0 to 1)
 let temperature = 0.6
