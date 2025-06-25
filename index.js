ff5ff0bcd445727ef5ab9834fbcef854578a90a6
Sandali Ranahewa <sandaliranahewa@gmail.com>
2025-06-25 07:21:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { generateTextAndImage } from "./utils.js"
 
-let name = "ashoka"
+let name = "sandaliz"
 
-let favoriteActivity = "Gardening"
+let favoriteActivity = "learning something new"
 
-let favoritePlace = "home"
+let favoritePlace = "anywhere with my favourite people"
 
 // The higher temperature, the more random & experimental output(from 0 to 1)
-let temperature = 1
+let temperature = 0.5
 
 generateTextAndImage(name, favoriteActivity, favoritePlace, temperature)
